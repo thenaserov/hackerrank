c32d2733dbacdf6734d10f66ee36cbec2c6d343e
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2025-05-15 13:13:06 +0330

    Create Functions.cpp

---
+++ Functions.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int semi;
+    if (a > b){
+        semi = a;
+    } else {
+        semi = b;
+    }
+    int final;
+    if (c > d){
+        final = c;
+    } else {
+        final = d;
+    }
+    
+    if (semi > final){
+        return semi;
+    } else {
+        return final;
+    }
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
