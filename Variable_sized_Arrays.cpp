d07102af7f60297cf2dcc2a49490bd8e04830157
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2025-05-16 15:39:44 +0330

    Update Variable_sized_Arrays.cpp

--- Variable_sized_Arrays.cpp
+++ Variable_sized_Arrays.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
-
+#include <sstream>
 using namespace std;
 
 int main() {
     int n, q;
     cin >> n >> q;
     vector<vector<int>> vecs;
+
     for(int count = 0; count < n; count++){
         int x;
         cin >> x;
@@ -16,20 +17,23 @@ int main() {
         }
         vecs.push_back(numbers);
     }
+
     vector<vector<int>> subs;
     for (int i = 0; i < q; i++){
-        int o, p;
-        cin >> o >> p;
+        int q, p;
+        cin >> q >> p;
         vector<int> tmp;
-        tmp.push_back(o);
+        tmp.push_back(q);
         tmp.push_back(p);
         subs.push_back(tmp);
     }
     
+    vector<int> res;
     for (int i = 0; i < subs.size(); i++){
-        cout << vecs.at(i).at(subs.at(i).at(1)) << "\n";
+        res.push_back(vecs.at(subs.at(i).at(0)).at(subs.at(i).at(1)));
     }
-    
+    for (int i = 0; i < res.size(); i++){
+        cout << res.at(i) << "\n";
+    }
     return 0;
 }
-
