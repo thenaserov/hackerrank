59b635b20a42b3242936b270573201c6d4a19625
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2025-05-23 11:07:51 +0330

    [WIP] Create Print_Pretty.cpp

---
+++ Print_Pretty.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int T; cin >> T;
+    cout << setiosflags(ios::uppercase);
+    cout << setw(0xf) << internal;
+    while(T--) {
+        double A; cin >> A;
+        double B; cin >> B;
+        double C; cin >> C;
+
+        /* Enter your code here */
+        cout << hex << showbase << uppercase << A << endl;
+        cout << fixed << setfill('_') << setw(15) << setprecision(2) << B << endl;
+        cout << scientific << uppercase << C << endl;
+            
+    }
+    return 0;
+}
