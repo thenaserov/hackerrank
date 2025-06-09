1ad0954c1d78d30cd97b4fb4eb017576c7d04fa1
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2025-06-09 15:19:31 +0330

    Update Print_Pretty.cpp

--- Print_Pretty.cpp
+++ Print_Pretty.cpp
@@ -14,7 +14,7 @@ int main() {
         double C; cin >> C;
 
         /* Enter your code here */
-      cout << left << showbase << hex << A << endl;
+      cout << left << showbase << hex << nouppercase << static_cast<int>(A) << endl;
       cout << right << setw(15) << setfill('_') << showpos << fixed << setprecision(2) << B << endl;
       cout << scientific << uppercase << noshowpos << setprecision(9) << C << endl;
             
